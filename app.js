0b72ca104d3c79ad51e16dd11ccfcf5455f3ff45
Kizito Akhilome <kizito@akhilo.me>
2018-09-11 09:52:23 +0100

    Make endpoint for adding new fortunes to the db
    - install and use body-parser
    - make use of the built-in fs module

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+const fs = require('fs');
 const express = require('express');
+const bodyParser = require('body-parser');
 const fortunes = require('./data/fortunes');
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.get('/fortunes', (req, res) => {
   res.json(fortunes);
@@ -13,6 +17,28 @@ app.get('/fortunes/random', (req, res) => {
 
 app.get('/fortunes/:id', (req, res) => {
   res.json(fortunes.find(fortune => fortune.id == req.params.id));
-})
+});
+
+app.post('/fortunes', (req, res) => {
+  // Get data out of request body
+  const { message, luckyNumber, spiritAnimal } = req.body;
+  // Get all ids of every fortune already in db
+  const fortuneIds = fortunes.map(fortune => fortune.id);
+  // Make new fortune based on data sent by user
+  const fortune = { 
+    id: (fortuneIds.length ? Math.max(...fortuneIds) : 0) + 1,
+    message: message,
+    luckyNumber: luckyNumber, 
+    spiritAnimal: spiritAnimal
+  };
+
+  const newFortunes = fortunes.concat(fortune);
+  // Update the 'database'
+  fs.writeFile('./data/fortunes.json', JSON.stringify(newFortunes), err => {
+    console.log(err)
+  });
+  
+  res.json(newFortunes);
+});
 
 module.exports = app;
